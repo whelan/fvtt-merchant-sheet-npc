575044949396b624bfd5e204992feb9cffc94c01
Lars Nørregaard <larsmhansen@gmail.com>
2021-04-18 22:43:25 +0200

    Removed comments
    Signed-off-by: Lars Nørregaard <larsmhansen@gmail.com>

--- scripts/merchant.js
+++ scripts/merchant.js
@@ -1165,97 +1165,9 @@ Hooks.once("init", () => {
             errorMessageToActor(buyer, `Not enough funds to purchase item.`);
             return;
         }
-        // const conversionRates = {
-        //     "pp": 1,
-        //     "gp": CONFIG.DND5E.currencyConversion.gp.each,
-        //     "ep": CONFIG.DND5E.currencyConversion.ep.each,
-        //     "sp": CONFIG.DND5E.currencyConversion.sp.each,
-        //     "cp": CONFIG.DND5E.currencyConversion.cp.each
-        // };
-        //
-        // const compensationCurrency = {"pp": "gp", "gp": "ep", "ep": "sp", "sp": "cp"};
-        //
-        // let itemCostInPlatinum = itemCostInGold / conversionRates["gp"]
-        // console.log(`itemCostInGold : ${itemCostInGold}`);
-        // console.log(`itemCostInPlatinum : ${itemCostInPlatinum}`);
-        // console.log(`conversionRates["gp"] : ${conversionRates["gp"]}`);
-        // console.log(`conversionRates["ep"] : ${conversionRates["ep"]}`);
-        //
-        // let buyerFundsAsPlatinum = buyerFunds["pp"];
-        // buyerFundsAsPlatinum += buyerFunds["gp"] / conversionRates["gp"];
-        // buyerFundsAsPlatinum += buyerFunds["ep"] / conversionRates["gp"] / conversionRates["ep"];
-        // buyerFundsAsPlatinum += buyerFunds["sp"] / conversionRates["gp"] / conversionRates["ep"] / conversionRates["sp"];
-        // buyerFundsAsPlatinum += buyerFunds["cp"] / conversionRates["gp"] / conversionRates["ep"] / conversionRates["sp"] / conversionRates["cp"];
-        //
-        // // console.log(`buyerFundsAsPlatinum : ${buyerFundsAsPlatinum}`);
-        //
-        //
-        // let convertCurrency = game.settings.get("merchantsheetnpc", "convertCurrency");
-        //
-        // if (convertCurrency) {
-        //     buyerFundsAsPlatinum -= itemCostInPlatinum;
-        //
-        //     // Remove every coin we have
-        //     for (let currency in buyerFunds) {
-        //         buyerFunds[currency] = 0
-        //     }
-        //
-        //     // Give us fractions of platinum coins, which will be smoothed out below
-        //     buyerFunds["pp"] = buyerFundsAsPlatinum
-        //
-        // } else {
-        //     // We just pay in partial platinum.
-        //     // We dont care if we get partial coins or negative once because we compensate later
-        //     buyerFunds["pp"] -= itemCostInPlatinum
-        //
-        //     // Now we exchange all negative funds with coins of lower value
-        //     // We dont need to care about running out of money because we checked that earlier
-        //     for (let currency in buyerFunds) {
-        //         let amount = buyerFunds[currency]
-        //         // console.log(`${currency} : ${amount}`);
-        //         if (amount >= 0) continue;
-        //
-        //         // If we have ever so slightly negative cp, it is likely due to floating point error
-        //         // We dont care and just give it to the player
-        //         if (currency == "cp") {
-        //             buyerFunds["cp"] = 0;
-        //             continue;
-        //         }
-        //
-        //         let compCurrency = compensationCurrency[currency]
-        //
-        //         buyerFunds[currency] = 0;
-        //         buyerFunds[compCurrency] += amount * conversionRates[compCurrency]; // amount is a negative value so we add it
-        //         // console.log(`Substracted: ${amount * conversionRates[compCurrency]} ${compCurrency}`);
-        //     }
-        // }
         console.log('Subtract from actor');
 
         currencyCalculator.subtractAmountFromActor(buyer,buyerFunds,itemCostInGold);
-        // console.log(`Smoothing out`);
-        // Finally we exchange partial coins with as little change as possible
-        // for (let currency in buyerFunds) {
-        //     let amount = buyerFunds[currency]
-        //
-        //     // console.log(`${currency} : ${amount}: ${conversionRates[currency]}`);
-        //
-        //     // We round to 5 decimals. 1 pp is 1000cp, so 5 decimals always rounds good enough
-        //     // We need to round because otherwise we get 15.99999999999918 instead of 16 due to floating point precision
-        //     // If we would floor 15.99999999999918 everything explodes
-        //     let newFund = Math.floor(Math.round(amount * 1e5) / 1e5);
-        //     buyerFunds[currency] = newFund;
-        //
-        //     // console.log(`New Buyer funds ${currency}: ${buyerFunds[currency]}`);
-        //     let compCurrency = compensationCurrency[currency]
-        //
-        //     // We dont care about fractions of CP
-        //     if (currency != "cp") {
-        //         // We calculate the amount of lower currency we get for the fraction of higher currency we have
-        //         let toAdd = Math.round((amount - newFund) * 1e5) / 1e5 * conversionRates[compCurrency]
-        //         buyerFunds[compCurrency] += toAdd
-        //         // console.log(`Added ${toAdd} to ${compCurrency} it is now ${buyerFunds[compCurrency]}`);
-        //     }
-        // }
 
         // Update buyer's funds
 
